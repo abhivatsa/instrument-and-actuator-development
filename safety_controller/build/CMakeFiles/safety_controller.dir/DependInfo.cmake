853e5fbc596c95bd64d16393e54e94099236f0d5
abhishek vatsa <abhishekvatsa1608@gmail.com>
2024-01-31 12:20:20 +0530

    working on gitignore

--- safety_controller/build/CMakeFiles/safety_controller.dir/DependInfo.cmake
+++ safety_controller/build/CMakeFiles/safety_controller.dir/DependInfo.cmake
@@ -11,8 +11,12 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/abhishek/Documents/instrument-devel-app/safety_controller/safety_controller.cpp" "CMakeFiles/safety_controller.dir/safety_controller.cpp.o" "gcc" "CMakeFiles/safety_controller.dir/safety_controller.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
